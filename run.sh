97d4f937e372977227efd9a62338bb0292c32bf8
zzhuncle <3294175303@qq.com>
2023-05-03 16:42:59 +0800

    lab1-gdb

--- run.sh
+++ run.sh
@@ -6,3 +6,11 @@ sudo dpkg -i libnettle7_3.5.1+really3.5.1-2ubuntu0.2_amd64.deb
 
 wget http://archive.ubuntu.com/ubuntu/pool/main/q/qemu/qemu-system-misc_4.2-3ubuntu6_amd64.deb
 sudo dpkg -i qemu-system-misc_4.2-3ubuntu6_amd64.deb
+
+git reflog # 查看每一次命令
+
+git reset --hard HEAD^ # 回滚到上个版本
+
+git branch -a # 查看分支
+
+riscv64-unknown-elf-gdb # 调试
